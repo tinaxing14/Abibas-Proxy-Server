2aebcafeb6104d5652285246ac480359edb80978
Tina Xing <tinaxing14@gmail.com>
2020-06-02 20:23:46 -0400

    ready to deploy to ec2" ;

--- server.js
+++ server.js
@@ -2,17 +2,28 @@ const express = require('express');
 const cors = require('cors');
 const httpProxyMiddleware = require('http-proxy-middleware');
 const morgan = require('morgan');
+const fetch = require('node-fetch');
 
 const app = express();
-const PORT = process.env.PROXY_PORT || 3000;
+const PORT = process.env.PROXY_PORT || 3007;
 
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan('tiny'));
 app.use(express.static('./'));
+app.use(express.json);
+app.use(express.urlencoded());
 
-// app.get('/api/models/${this.state.productcode}/reviews', 
-//   axios.get('http://localhost:3003/api/models/${this.state.productcode}/reviews`)
-//     .then(response => response.data)
-//   )
+const ymalhost = '3.22.100.126'
+
+// app.use('/api/shoes/:id/relatedproducts', (req, res) => {
+//   const url= `${ymalhost}/api/shoes${req.url}`
+//   fetch(url)
+//     .then(response => response.json())
+//     .then(json => res.json(json))
+//     .catch(err => {
+//       console.log(err);
+
+//     });
+// })
 
 app.listen(PORT, () => console.log('Listening on port: ' + PORT)); 
